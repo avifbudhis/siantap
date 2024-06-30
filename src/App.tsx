67a7a4cecfa6480813197c1aee06191d9437d084
The White Ants <153772038+avifbudhis@users.noreply.github.com>
2024-07-01 00:21:19 +0700

    update view

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Analytics } from "@vercel/analytics/react";
-import {
-  WebAppProvider,
-  MainButton,
-  useShowPopup,
-} from "@vkruglikov/react-telegram-web-app";
+import { Analytics, track } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+import WebApp from "@twa-dev/sdk";
 
 function App() {
   const [count, setCount] = useState(0);
-  const showPopup = useShowPopup();
 
-  const handleClick = () =>
-    showPopup({
-      message: "Hello, I am popup",
-    });
+  const handleCount = () => {
+    setCount((count) => count + 1);
+    track("count");
+  };
 
   return (
     <>
-      <WebAppProvider
-        options={{
-          smoothButtonsTransition: true,
-        }}
-      >
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
+      <div>
+        <h1>Siantap Airdrop</h1>
         <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
+          <button onClick={handleCount}>count is {count}</button>
         </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-        <Analytics />
-        <MainButton text="SHOW POPUP" onClick={handleClick} />
-      </WebAppProvider>
+      </div>
+      <Analytics />
+      <SpeedInsights />
     </>
   );
 }
